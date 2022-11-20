abd460170a3f7816db20b7af41d7633ae9d4edc7
Your Name <you@example.com>
2022-11-20 21:26:55 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+<meta http-equiv="Pragma" content="no-cache" />
+<meta http-equiv="Expires" content="0" />
+<html>
+  <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+<meta http-equiv="Pragma" content="no-cache" />
+<meta http-equiv="Expires" content="0" />
+<head>
+  <meta http-equiv='cache-control' content='no-cache'>
+<meta http-equiv='expires' content='0'>
+<meta http-equiv='pragma' content='no-cache'>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+
+img {
+  display: block;
+  margin-left: auto;
+  width:500px;
+  height:300px;
+  margin-right: auto;
+}
+h2 {text-align: center;
+ font-size: 20px;}
+input[type=text], select {
+  width: 68%;
+  align-self: center;
+  background-color: #ffffff;
+  color: rgb(0, 0, 0);
+  padding: 7px 20px;
+  margin: 2px 0;
+  border: none;
+  border-radius: 14px;
+  cursor: pointer;
+  margin-left:auto;
+  margin-right:auto;
+  display:block;
+  margin-top:1%;
+  margin-bottom:0%;
+}
+
+input[type=submit] {
+  width: 70%;
+  align-self: center;
+  background-color: #ffe552;
+  color: rgb(0, 0, 0);
+  padding: 7px 20px;
+  margin: 2px 0;
+  border: none;
+  border-radius: 14px;
+  cursor: pointer;
+  margin-left:auto;
+  margin-right:auto;
+  display:block;
+  margin-top:1%;
+  margin-bottom:0%;
+  
+  
+  
+}
+
+input[type=submit]:hover {
+  background-color: #ffe552;
+}
+
+div {
+  border-radius: 10px;
+  background-color: #ffe552;
+  padding: 20px;
+}
+h1 {
+  font-size: 10px;
+  text-align: center;
+}
+</style>
+</head>
+<body>
+
+<h2 style="color:white">WELCOME TO CAPITAL BETTING BOT</h2>
+
+<img src="src/logo.png" alt="Paris" style="width:50%;">
+
+ 
+
+     
+    <input type="text"  placeholder="USERNAME" style="border-radius: 10px;"  >
+    <input type="text"  placeholder="PASSWORD" style="border-radius: 10px;"  >
+    
+   <input type="submit" value="LOGIN" onclick="getVal()">
+
+     <input type="submit" value="REGISTER" onclick="location.href='getid.html'" >
+     <input type="submit" name="b1" value="CHECK SERVER STATUS" onclick="location.href='signup.html'">
+  <input type="submit" name="b1" value=" WhatsApp us (Live Assessment)"
+ onclick="location.href='https://api.whatsapp.com/send?phone=13656016616'">
+ 
+ <input type="submit" name="b1" value="What is TRN id?"
+ onclick="location.href='/upi/trn.html'">
+  <input type="submit" name="b1" value="SEE RECENT TRADES (TELEGRAM CHANNEL)" onclick="location.href='https://t.me/algo_Tradeing'">
+ <input type="submit" name="b1" value="RESENT TRADEING RESULTS"
+ onclick="location.href='result.html'">
+ <input type="submit" name="b1" value="SERVER LOG"
+ onclick="location.href='/status/log.html'">
+  <input type="submit" name="b1" value="TRY ONE TRADE  FOR FREE NOW"
+ onclick="location.href='/try.html'">
+<a href="/">
+  
+<a href="/">
+  </form>
+</div>
+
+<body style="background-color:black;">
+ <h1 style="color:white">Greetings from Capital Betting</h1>
+</html>
+<html>
+<head>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia">
+<style>
+body {
+  font-family: "Sofia", sans-serif;
+}
+</style>
+ 
+
+<script type="text/javascript" src="func.js?updated=1234567890>"></script>
+ <script>
+cache.delete(request, {options}).then(function(found) {
+  // your cache entry has been deleted if found
+});
+caches.open('v1').then(function(cache) {
+  cache.delete('/images/image.png').then(function(response) {
+    someUIUpdateFunction();
+
+  });
+})
+ </script>
+
+  
+
+
