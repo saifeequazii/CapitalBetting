2c69e221fc9f161a36bf48ab993e470e4a45cfe2
Your Name <you@example.com>
2022-11-25 20:57:13 +0530

    update

--- signup.php
+++ signup.php
@@ -10,7 +10,7 @@ $c = 'element_'.$_POST["uname"];
          $redis->set($_POST["uname"], $_POST["pword"]); 
          $redis->set($b, $_POST["name"]); 
          $redis->set($a, $_POST["mnum"]); 
-         $redis->set($c, "0"); 
+         $redis->set($c, "NOT ACTIVATED"); 
          $redis->set("foo", "bar");
          $db_name = print_r($redis->get("username"),true);
          echo $db_name;
