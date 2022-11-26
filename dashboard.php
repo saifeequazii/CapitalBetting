aa92da18b2635f25ca5c77a3bfe0f57d904617f5
Your Name <you@example.com>
2022-11-26 20:29:56 +0530

    update

--- dashboard.php
+++ dashboard.php
@@ -124,8 +124,7 @@ input[type=text], select {
   margin-bottom:0%;
 }
 input[type=submit] {
-  width: 20%;
-  height: 50%;
+
   align-self: center;
   background-color: #ffe552;
   color: rgb(0, 0, 0);
