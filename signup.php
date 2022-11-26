8be5d23f18937615ef9ad595c22d371b49ca7263
Your Name <you@example.com>
2022-11-26 21:13:10 +0530

    update

--- signup.php
+++ signup.php
@@ -17,14 +17,9 @@ $d = 'activator_'.$_POST["uname"];
          $redis->set("foo", "bar");
          $db_name = print_r($redis->get("username"),true);
          echo $db_name;
-         echo <<< END_OF_TEXT
-         <script>
-         let xhr = new XMLHttpRequest();
-       fetch("https://api.telegram.org/bot5769765879:AAGSKonu0sNtUJ5lzV75cSbv9cNOdyr6tUc/sendMessage?chat_id=@capitalbetting&text=HAS SIGNED UP FOR CAPITALBETTING WELCOME TO THE TEAM");
-       
-               </script>
-     END_OF_TEXT;
-
+         
+         header("Location: index.php");
+exit();
 
 
       
