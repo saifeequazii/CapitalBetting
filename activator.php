2fa8fcc69c2b2337f43bc9583b4741656111a506
Your Name <you@example.com>
2022-11-27 21:39:52 +0530

    update

--- activator.php
+++ activator.php
@@ -4,9 +4,10 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia">
 <img src="src/logo.png" alt="Paris" style="width:50%;">
 <body style="background-color:black;">
-<h2>500₹ For earning rate 2.25₹ per minuts validity 48 hours Account bal should be equal or more then 500</h2>
+
 
 <input type="submit" value="pay 500" onclick="location.href='https://pmny.in/jrO38JLhd4On'" >
+<h2>700₹ For earning rate 3.25₹ per minuts validity 48 hours Account bal should be equal or more then 700</h2>
 <input type="submit" value="pay 700" onclick="location.href='https://pmny.in/ur43CJqhAAvv'" >
 <input type="submit" value="1000₹ For earning rate 4₹ per minuts validity 48 hours Account bal should be equal or more then 2000" onclick="location.href='https://pmny.in/EI7XpotlLALu'" >
 <input type="submit" value="2000₹ For earning rate 6₹ per minuts validity 48 hours Account bal should be equal or more then 5000" onclick="location.href='https://pmny.in/wIEa80yBdui6'" >
