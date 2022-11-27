9b9bb137fc4b1d7e2519228685d43417cdbd1dd9
Your Name <you@example.com>
2022-11-27 21:45:39 +0530

    update

--- activator.php
+++ activator.php
@@ -5,13 +5,13 @@
 <img src="src/logo.png" alt="Paris" style="width:50%;">
 <body style="background-color:black;">
 <h2>500₹ For earning rate 2.25₹ per minuts validity 48 hours Account bal should be equal or more then 500</h2>
-<input type="submit" value="pay 500" onclick="location.href='https://pmny.in/jrO38JLhd4On'" >
+<input type="submit" value="pay 500₹" onclick="location.href='https://pmny.in/jrO38JLhd4On'" >
 <h2>700₹ For earning rate 3.25₹ per minuts validity 48 hours Account bal should be equal or more then 700</h2>
-<input type="submit" value="pay 700" onclick="location.href='https://pmny.in/ur43CJqhAAvv'" >
+<input type="submit" value="pay 700₹" onclick="location.href='https://pmny.in/ur43CJqhAAvv'" >
 <h2>1000₹ For earning rate 4₹ per minuts validity 48 hours Account bal should be equal or more then 2000</h2>
-<input type="submit" value="pay 1000" onclick="location.href='https://pmny.in/EI7XpotlLALu'" >
+<input type="submit" value="pay 1000₹" onclick="location.href='https://pmny.in/EI7XpotlLALu'" >
 <h2>2000₹ For earning rate 6₹ per minuts validity 48 hours Account bal should be equal or more then 5000</h2>
-<input type="submit" value="pay 2000" onclick="location.href='https://pmny.in/wIEa80yBdui6'" >
+<input type="submit" value="pay 2000₹" onclick="location.href='https://pmny.in/wIEa80yBdui6'" >
 <h2>5000₹ For earning rate 25₹ per minuts validity 48 hours Account bal should be equal or more then 10000</h2>
 <input type="submit" value="pay 5000₹" onclick="location.href='https://pmny.in/TI0XsoHlvyZe'" >
 </body>
