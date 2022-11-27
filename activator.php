4ae0bf0b633b53cf2add8f6031845ce8f57c683a
Your Name <you@example.com>
2022-11-27 20:44:56 +0530

    update

--- activator.php
+++ activator.php
@@ -3,7 +3,8 @@
 <body>
 <img src="src/logo.png" alt="Paris" style="width:50%;">
 <body style="background-color:black;">
-<input type="submit" value="500₹ For earning rate 1.25₹ per minuts validity 48 hours" onclick="location.href='register.php'" >
+<input type="submit" value="500₹ For earning rate 2.25₹ per minuts validity 48 hours" onclick="location.href='register.php'" >
+<input type="submit" value="700₹ For earning rate 3₹ per minuts validity 48 hours" onclick="location.href='register.php'" >
 </body>
 </html> 
 <style>
