da01942e789934ad063c378fec854acd6bf151fa
Your Name <you@example.com>
2022-11-28 18:44:23 +0530

    update

--- dashboard.php
+++ dashboard.php
@@ -33,7 +33,7 @@ else {
             #echo ' <h2 style="color:black">Welcome  </h2> '<i>$db_name</i> ;
             echo <<< END_OF_TEXT
             <h1>WELCOME $name</h1>
-            <h2> $nam</h2>
+            
             <h1>Current Earning rate:- $element / min</h1><input type="submit" value="upgrade plan" onclick="myFunction()" >
             <script>
             function myFunction() {
