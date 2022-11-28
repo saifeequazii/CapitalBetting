768cba748a31c9b6258449229e748d2acee1137d
Your Name <you@example.com>
2022-11-28 14:36:00 +0530

    update

--- redis_bug/run.sh
+++ redis_bug/run.sh
@@ -3,8 +3,8 @@ do
 sleep 3
 if redis-cli -h ssh.capitalbeting.com set foo bar |grep read
 then
-docker-compose up
-docker-compose down -d 
+docker-compose up -d
+docker-compose down 
 sleep 1
 else
 docker-compose up -d
