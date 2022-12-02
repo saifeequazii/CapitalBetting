b8ce48dd31e0b802c46352a24afb3506ed740c6a
mayanktryroll <mayank@tryroll.com>
2022-12-02 16:31:34 +0530

    update

--- activator.php
+++ activator.php
@@ -4,6 +4,8 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia">
 <img src="src/logo.png" alt="Paris" style="width:50%;">
 <body style="background-color:black;">
+<h2>(FOR TESTING ONLY ONE TIME)50₹ For earning rate 2.25₹ per minuts validity 1 minuts Account bal should be equal or more then 500</h2>
+<input type="submit" value="pay 500₹" onclick="location.href='https://pmny.in/jrO38JLhd4On'" >
 <h2>500₹ For earning rate 2.25₹ per minuts validity 48 hours Account bal should be equal or more then 500</h2>
 <input type="submit" value="pay 500₹" onclick="location.href='https://pmny.in/jrO38JLhd4On'" >
 <h2>700₹ For earning rate 3.25₹ per minuts validity 48 hours Account bal should be equal or more then 700</h2>
@@ -22,7 +24,7 @@ img {
   margin-left: auto;
   position: relative;
   margin-right: auto;
-  max-width: 500px;
+  max-width: 500px; 
   height: 500px;
   width: auto;
   border: solid black;
