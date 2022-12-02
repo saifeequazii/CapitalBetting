3411510c1d04051721058d878acbd1283a37207f
mayanktryroll <mayank@tryroll.com>
2022-12-02 16:22:57 +0530

    update

--- test.php
+++ test.php
@@ -3,7 +3,7 @@
    $redis = new Redis(); 
    $redis->connect('db', 6379); 
    echo "Connection to server sucessfully"; 
-   //check whether server is running or not 
+   
    echo "Server is running: ";
    $redis->set("faizii", "CAPITALBETING"); 
    if ($redis->ping()) {
